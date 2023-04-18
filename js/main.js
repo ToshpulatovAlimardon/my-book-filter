f27f3de14f4cdc948fa29032c70d669c8cf8da79
Alimardon Toshpo'latov <alimardoncoder001@gmail.com>
2023-04-18 08:04:47 +0500

    changed main js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const elForm = document.querySelector("#book-form");
+const elBtnGroup = document.querySelector(".btn-group");
+const elListGroup = document.querySelector(".list-group");
+
+let newArr = [];
+
+elForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+43:34
